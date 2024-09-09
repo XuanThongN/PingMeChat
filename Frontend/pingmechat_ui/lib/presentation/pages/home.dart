6d8164420cc6a7a195225d7a93a5fad66b9d31ce
XuanThongN <thong.xuan.vft@gmail.com>
2024-09-10 01:18:10 +0700

    fix: UI

--- Frontend/pingmechat_ui/lib/presentation/pages/home.dart
+++ Frontend/pingmechat_ui/lib/presentation/pages/home.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:pingmechat_ui/config/theme.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:pingmechat_ui/presentation/pages/call_tab.dart';
-import 'package:pingmechat_ui/presentation/pages/chat_page.dart';
 import 'package:pingmechat_ui/presentation/pages/contact_tab.dart';
 import 'package:pingmechat_ui/presentation/pages/message_tab.dart';
 import 'package:pingmechat_ui/presentation/pages/settting_tab.dart';
 
 import '../widgets/custom_icon.dart';
-import 'create_group_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -42,7 +37,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: SafeArea(
-        child: Column(
+        child: Column( // Giải thích: 
           children: [
             Expanded(child: _tabs[_selectedIndex]),
             _buildBottomNavBar(),
@@ -62,19 +57,23 @@ class _HomePageState extends State<HomePage> {
       onTap: _onItemTapped,
       items: [
         BottomNavigationBarItem(
-          icon: CustomSvgIcon(svgPath: 'assets/icons/Message.svg'),
+          icon: CustomSvgIcon(svgPath: 'assets/icons/Message.svg', color: AppColors.tertiary),
+          activeIcon: CustomSvgIcon(svgPath: 'assets/icons/Message.svg', color: AppColors.primary),
           label: AppLocalizations.of(context)!.message,
         ),
         BottomNavigationBarItem(
           icon: CustomSvgIcon(svgPath: 'assets/icons/Call.svg'),
+          activeIcon: CustomSvgIcon(svgPath: 'assets/icons/Call.svg', color: AppColors.primary),
           label: AppLocalizations.of(context)!.calls,
         ),
         BottomNavigationBarItem(
           icon: CustomSvgIcon(svgPath: 'assets/icons/user.svg'),
+          activeIcon: CustomSvgIcon(svgPath: 'assets/icons/user.svg', color: AppColors.primary),
           label: AppLocalizations.of(context)!.contacts,
         ),
         BottomNavigationBarItem(
           icon: CustomSvgIcon(svgPath: 'assets/icons/settings.svg'),
+          activeIcon: CustomSvgIcon(svgPath: 'assets/icons/settings.svg', color: AppColors.primary),
           label: AppLocalizations.of(context)!.settings,
         ),
       ],
