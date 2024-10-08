e58f3c39750d1f14b62ab99190df8eb8f65d3a58
XuanThongN <100119600+XuanThongN@users.noreply.github.com>
2024-10-08 02:42:25 +0000

    feat: add model and interaction matrix

--- Recommend_model/model_recommend_api.py
+++ Recommend_model/model_recommend_api.py
@@ -1,23 +1,44 @@
+
+# !pip install flask
+
 from flask import Flask, request, jsonify
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 
-# Load the KNN model
-with open('knn_model.pkl', 'rb') as model_file:
-    knn_model = pickle.load(model_file)
+# Load mô hình KNN và ma trận tương tác người dùng
+model_knn = pickle.load(open('knn_model.pkl', 'rb'))
+user_interaction_matrix = pd.read_csv('user_interaction_matrix.csv', index_col='UserId')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    features = data['features']
-    
-    # Ensure the input is in the correct format
-    if not isinstance(features, list):
-        return jsonify({'error': 'Invalid input format. Expected a list of features.'}), 400
-    
-    prediction = knn_model.predict([features])
-    return jsonify({'prediction': prediction.tolist()})
+
+def get_recommendations_knn(user_id, model_knn, user_interaction_matrix, top_n=20):
+    """
+    Lấy danh sách các user được đề xuất cho một user cụ thể sử dụng KNN.
+    """
+    try:
+        user_interactions = user_interaction_matrix.loc[[user_id]]
+
+        if user_interactions.empty:
+            return []
+
+        distances, indices = model_knn.kneighbors(user_interactions, n_neighbors=top_n + 1)
+        recommended_user_indices = indices.flatten()[1:]
+        recommended_user_ids = user_interaction_matrix.index[recommended_user_indices].tolist()
+        return recommended_user_ids
+    except KeyError:
+        return []
+
+
+@app.route('/recommend', methods=['GET'])
+def recommend_users():
+    user_id = request.args.get('user_id')
+    if user_id:
+        recommended_users = get_recommendations_knn(user_id, model_knn, user_interaction_matrix)
+        return jsonify({'recommendations': recommended_users})
+    else:
+        return jsonify({'error': 'Missing user_id parameter'}), 400
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
