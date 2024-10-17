2f6f42591a3a0d5f3259a737f492c8077c402665
THONGNX-G10\ThongNX <thongnx@rikkeisoft.com>
2024-10-17 09:48:28 +0700

    fix: load api update avatar swagger

--- Backend/PingMeChat.CMS.Application/Feature/Service/Users/Dto/UpdateProfileDto.cs
+++ Backend/PingMeChat.CMS.Application/Feature/Service/Users/Dto/UpdateProfileDto.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.Http;
+
 namespace PingMeChat.CMS.Application.Feature.Service.Users.Dto
 {
     public class UpdateProfileDto
     {
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
+    }
+
+    public class UpdateAvatarDto {
+        public IFormFile avatar { get; set; }
     }
 }
